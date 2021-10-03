9a4708c8bd7a311a37a4c5a1c24d6847199e03b4
“yourGithubUsername” <your@emailadd.com>
2021-10-03 22:01:28 +0100

    enhance pages

--- client/static/theme.js
+++ client/static/theme.js
@@ -1,5 +1,23 @@
-const moonIcon = document.querySelector('#moon-icon');
+//For dark/light theme toggle
+const moonIcon = document.querySelector('.sun-moon-theme');
 
 moonIcon.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme')
 })
+////////////////////////////////////////////////
+
+////for RESPONSIVENESS
+//menu-list
+const menuList = document.querySelector('#menu-list');
+
+menuList.style.maxHeight = "0px";
+
+function toggleMenu() {
+    if(menuList.style.maxHeight == "0px") {
+        menuList.style.maxHeight = "100px"
+    } else {
+        menuList.style.maxHeight = "0px"
+    }
+}
+
+
