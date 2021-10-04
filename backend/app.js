8b19baad5040ef31f19fe86edb9e5ebf9b1390d7
Saja A <saja.aabith@gmail.comxitcdgit config --global user.name saja-aabith>
2021-10-04 01:51:00 +0100

    first commit, where all the basic layout of the codes have been added

---
+++ backend/app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const cors = require('cors')
+
+
+let journals = [
+  {
+    title:'My First Post',
+    body: 'blahblahblah',
+    timeCreated: 'timestamp',
+    comments: [
+      {
+        body:'Nice one!',
+        timeCreated: 'timestamp',
+      },
+      {
+        body:'Great!',
+        timeCreated: 'timestamp',
+      }
+    ]
+  }
+]
+
+
+
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.get('/posts', (req, res) => {
+//   let weatherResult = weather[req.query.city]
+//   if (weatherResult) {
+//     res.send(weatherResult)
+//   } else {
+//     res.send('Post not found')}
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
