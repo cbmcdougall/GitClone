a6d39a0b0b75d2a6140ca0993eacc29e1d6d7d10
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-10-04 12:40:17 +0100

    modify to retrieve data from backend JSON

--- client/static/index.js
+++ client/static/index.js
@@ -12,37 +12,23 @@
 // })
 
 // Display recent posts, redirect to entry.html for the post clicked
-examplePost = [{
-    "id": "2",
-    "title": "Hello world!",
-    "text": "This is an example post",
-    "date": "04/10/2021",
-    "thumbsUp": "2",
-    "thumbsDown": "0",
-    "comments": [{
-        body: "hello there!",
-        dateAdded: "04/10/2021"
-    }],
-},{
-    "id": "3",
-    "title": "Hello again!",
-    "text": "This is another example post",
-    "date": "04/10/2021",
-    "thumbsUp": "0",
-    "thumbsDown": "3",
-    "comments": []
-}];
+fetch("../../backend/data.json")
+  .then(resp => resp.json())
+  .then(data => renderPosts(data))
+  .catch(err => console.log(err));
 
-examplePost.forEach( post=> {
-    const postSection = document.createElement("section");
-    const postLink = document.createElement("A");
-    const postTitle = document.createTextNode(`${post.title}`);
-    postLink.setAttribute("href", "./static/entry.html");
-    postLink.addEventListener("click", () => {
-        // store the post data for entry.html to receive
-        sessionStorage.journalPost = JSON.stringify(post);
+function renderPosts(data){
+    data.forEach( post=> {
+        const postSection = document.createElement("section");
+        const postLink = document.createElement("A");
+        const postTitle = document.createTextNode(`${post.title}`);
+        postLink.setAttribute("href", "./static/entry.html");
+        postLink.addEventListener("click", () => {
+            // store the post data for entry.html to receive
+            sessionStorage.journalPost = JSON.stringify(post);
+        })
+        postLink.appendChild(postTitle);
+        postSection.appendChild(postLink);
+        document.body.appendChild(postSection);
     })
-    postLink.appendChild(postTitle);
-    postSection.appendChild(postLink);
-    document.body.appendChild(postSection);
-})
+}
