bcc5c11c34aefd8633272e2bbc8450abd2228830
“yourGithubUsername” <your@emailadd.com>
2021-10-05 21:47:11 +0100

    searc bar functioning ex

--- client/static/js/theme.js
+++ client/static/js/theme.js
@@ -11,14 +11,95 @@ moonIcon.addEventListener('click', () => {
 })
 ////////////////////////////////////////////////
 ///Search bar
-const searchContainer = document.querySelector(".search-container");
-const elementSearchContainer = searchContainer.querySelector(".element-search-container");
-const searchBar = searchContainer.querySelector("#searchBar");
 
-searchBar.onkeyup = (e) => {
-    console.log(e.target.value)
+let suggestions = [
+    "ant", "bird", "cat", "dog", "elephant", "fish", "goat", "horse", "igloo", "jackal", "kangeroo", "lizard", "monkey", "nobody", "octopus",
+    "pig", "queen", "rat", "snake", "umbrella", "venom", "whale", "xylophone", "yacht", "zebra", "animal", "bat", "banana", "blood", "beautiful"
+]
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
+const icon = searchWrapper.querySelector(".icon");
+let linkTag = searchWrapper.querySelector("a");
+let webLink;
+// if user press any key and release
+inputBox.onkeyup = (e)=>{
+    let userData = e.target.value; //user enetered data
+    let emptyArray = [];
+    if(userData){
+        icon.onclick = ()=>{
+            webLink = `https://www.google.com/search?q=${userData}`;
+            linkTag.setAttribute("href", webLink);
+            linkTag.click();
+        }
+        emptyArray = suggestions.filter((data)=>{
+            //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+        });
+        emptyArray = emptyArray.map((data)=>{
+            // passing return data inside li tag
+            return data = `<li>${data}</li>`;
+        });
+        searchWrapper.classList.add("active"); //show autocomplete box
+        showSuggestions(emptyArray);
+        let allList = suggBox.querySelectorAll("li");
+        for (let i = 0; i < allList.length; i++) {
+            //adding onclick attribute in all li tag
+            allList[i].setAttribute("onclick", "select(this)");
+        }
+    }else{
+        searchWrapper.classList.remove("active"); //hide autocomplete box
+    }
+}
+function select(element){
+    let selectData = element.textContent;
+    inputBox.value = selectData;
+    icon.onclick = ()=>{
+        webLink = `https://www.google.com/search?q=${selectData}`;
+        linkTag.setAttribute("href", webLink);
+        linkTag.click();
+    }
+    searchWrapper.classList.remove("active");
+}
+function showSuggestions(list){
+    let listData;
+    if(!list.length){
+        userValue = inputBox.value;
+        listData = `<li>${userValue}</li>`;
+    }else{
+      listData = list.join('');
+    }
+    suggBox.innerHTML = listData;
 }
 
+// searchBar.onkeyup = (e) => {
+//     let userData = e.target.value; //user enters data
+//     let emptyArray = [];
+//     if(userData){
+//         emptyArray = testArray.filter((data)=> {
+//             return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+//         });
+//         emptyArray = emptyArray.map((data)=> {
+//             return data = '<a href="#">' + data + '</a>';
+//         });
+//         console.log(emptyArray);
+//         resultsBox.classList.add("active");
+//     } else {
+
+//     }
+//     showSuggestions(emptyArray);
+// }
+
+// function showSuggestions(list){
+//     let listData;
+//     if(!list.length) {
+
+//     } else {
+//         listData = list.join()
+//     }
+//     resultsBox.innerHTML = listData;
+
+// }
 
 
 
