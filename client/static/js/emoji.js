0bc6fe9729b2e910bd706a6fba8571951dfdc78c
“yourGithubUsername” <your@emailadd.com>
2021-10-06 15:54:18 +0100

    search bar JS complete

--- client/static/js/emoji.js
+++ client/static/js/emoji.js
@@ -1,1 +1,68 @@
-const post = JSON.parse(sessionStorage.journalPost);
+// //Emoji reaction on entry.html
+// const post = JSON.parse(sessionStorage.journalPost);
+
+// const tUpEmo = document.querySelector("#emoji-thumbs-up");
+// const tDownEmo = document.querySelector("#emoji-thumbs-down");
+// const laughingEmo = document.querySelector("#emoji-laughing");
+
+// //When emoji is clicked then add number:
+// tUpEmo.addEventListener('click', () => {
+    
+// })
+
+
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const postId = urlParams.get('id');
+
+getPost(postId);
+
+let reactions = [
+    {
+        emoji: "thumbs-up",
+        apiCall: "thumbsUp"
+    },
+    {
+        emoji: "thumbs-down",
+        apiCall: "heart"
+    },
+    {
+        emoji: "grin-squint-tears",
+        apiCall: "angryFace"
+    }
+]
+
+reactions.forEach(reaction => {
+    let button = document.querySelector(`.emojis i.fa-${reaction.emoji}`);
+    button.addEventListener('click', (event) => {
+        updateReaction(reaction.apiCall,postId);
+    })
+})
+
+
+function getPost(id) {
+  fetch(`https://git-clone-blog.herokuapp.com/pushes/${id}`)
+    .then(function(res){
+      if(!res.ok) {
+        throw new Error("HTTP error " + res.status)
+      }
+      return res.json()
+    })
+    .then(function(data) {
+        updateUI(data);
+    })
+    .catch(err => console.log(err))
+}
+
+function updateReaction(reaction, id) {
+
+    fetch(`https://git-clone-blog.herokuapp.com/pushes/${id}/${reaction}`, {
+        method: "PUT"
+    }).then(res => {
+        if(!res.ok) {
+            throw new Error("HTTP error " + res.status)
+        }
+        location.reload();
+    }).catch(err => console.log(err));
+}
--- client/static/js/theme.js
+++ client/static/js/theme.js
@@ -12,65 +12,96 @@ moonIcon.addEventListener('click', () => {
 ////////////////////////////////////////////////
 ///Search bar
 
-let suggestions;
-fetch("https://git-clone-blog.herokuapp.com/pushes")
-    .then(resp => resp.json())
-    .then(data => fillSuggestions(data, suggestions))
-    .catch(err => console.log(err));
-
-function fillSuggestions(data,suggestions){
-        suggestions = data.map(post => {return post.title})
-}
-
 const searchWrapper = document.querySelector(".search-input");
 const inputBox = searchWrapper.querySelector("input");
 const suggBox = searchWrapper.querySelector(".autocom-box");
 const icon = searchWrapper.querySelector(".icon");
 let linkTag = searchWrapper.querySelector("a");
 let webLink;
-// if user press any key and release
-inputBox.onkeyup = (e)=>{
-    let userData = e.target.value; //user enetered data
-    let emptyArray = [];
-    if(userData){
-        icon.onclick = ()=>{
-            result = data.filter(post => {
-            userData.toLocaleLowerCase() === post.title.toLocaleLowerCase()
-            })
-            sessionStorage.journalPost = JSON.stringify(result);
-            linkTag.setAttribute("href", '.static/entry.html');
-            linkTag.click();
-            // webLink = `https://www.google.com/search?q=${userData}`; //>>>>>////CHANGE THIS TO INDVIDIUAL LINK
-            // linkTag.setAttribute("href", webLink);
-            // linkTag.click();
-        }
-        // emptyArray = suggestions.filter((data)=>{
-        //     //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
-        //     return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
-        // });
-        emptyArray = emptyArray.map((data)=>{
-            // passing return data inside li tag
-            return data = `<li>${data}</li>`;
-        });
-        searchWrapper.classList.add("active"); //show autocomplete box
-        showSuggestions(emptyArray);
-        let allList = suggBox.querySelectorAll("li");
-        for (let i = 0; i < allList.length; i++) {
-            //adding onclick attribute in all li tag
+
+fetch("https://git-clone-blog.herokuapp.com/pushes")
+    .then(resp => resp.json())
+    .then(data => fillSuggestions(data))
+    .catch(err => console.log(err));
+
+
+
+function fillSuggestions(data){
+    const suggestions = data.map(post => {return post.title})
+        
+
+
+    
+    // if user press any key and release
+    inputBox.onkeyup = (e)=>{
+        let userData = e.target.value; //user enetered data
+        let emptyArray = [];
+        if(userData){
+            emptyArray = suggestions.filter((data)=>{
+                //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
+                return data.toLowerCase().startsWith(userData.toLowerCase());
+            });
+            emptyArray = emptyArray.map((data)=>{
+                // passing return data inside li tag
+                return data = `<li>${data}</li>`;
+            });
+            // for (let i = 0; i < emptyArray.length; i++) {
+            //     emptyArray[i].onclick = () => {
+            //         let selectData = this.textContent
+            //         const result = data.filter(post => {
+            //             return selectData.toLowerCase() === post.title.toLowerCase()
+            //             })
+            //             sessionStorage.journalPost = JSON.stringify(result[0]);
+            //             window.location.href = "entry.html"
+            //     }
+            // }
+            searchWrapper.classList.add("active"); //show autocomplete box
+            showSuggestions(emptyArray);
+            let allList = suggBox.querySelectorAll("li");
+            for (let i = 0; i < allList.length; i++) {
+                //adding onclick attribute in all li tag
             allList[i].setAttribute("onclick", "select(this)");
+            //     allList[i].onclick = () => {
+            //         let selectData = this.textContent
+            //         const result = data.filter(post => {
+            //             return selectData.toLowerCase() === post.title.toLowerCase()
+            //             })
+            //             sessionStorage.journalPost = JSON.stringify(result[0]);
+            //             window.location.href = "entry.html"
+            //     }
+            }
+            icon.onclick = ()=>{
+                const result = data.filter(post => {
+                return inputBox.value.toLowerCase() === post.title.toLowerCase()
+                })
+                sessionStorage.journalPost = JSON.stringify(result[0]);
+                const path = window.location.pathname;
+                if (path==="/client/index.html"){
+                    // We're currently on homepage
+                    window.location.href = "./static/entry.html"
+                } else {
+                    // We're currently on entry.html
+                    location.reload()
+                }
+            }
+        }else{
+            searchWrapper.classList.remove("active"); //hide autocomplete box
         }
-    }else{
-        searchWrapper.classList.remove("active"); //hide autocomplete box
     }
 }
 function select(element){
     let selectData = element.textContent;
     inputBox.value = selectData;
-    icon.onclick = ()=>{
-        webLink = `https://www.google.com/search?q=${selectData}`;
-        linkTag.setAttribute("href", webLink);
-        linkTag.click();
-    }
+    userData = selectData;
+    // icon.onclick = ()=>{
+    //     const result = data.filter(post => {
+    //     return userData.toLowerCase() === post.title.toLowerCase()
+    //     })
+    //     sessionStorage.journalPost = JSON.stringify(result[0]);
+    //     window.location.href = "entry.html"
+        
+    
+    //}
     searchWrapper.classList.remove("active");
 }
 function showSuggestions(list){
