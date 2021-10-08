457921e336b64a2a93c2e2351c4232b1384960db
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-10-08 10:27:59 +0100

    increase test coverage

--- backend/tests/api-endpoints.spec.js
+++ backend/tests/api-endpoints.spec.js
@@ -15,6 +15,14 @@ describe('api routes', () => {
         api.close(done);
     });
 
+    test("GET / tells you to go to /pushes for posts", (done) => {
+        request(api)
+            .get('/')
+            .set('Accept', 'application/json')
+            .expect('Visit /pushes to get all the blog posts!')
+            .expect(200, done)
+    })
+
     test("GET /pushes returns all the stored blog posts", (done) => {
         request(api)
             .get('/pushes')
@@ -31,7 +39,7 @@ describe('api routes', () => {
                 "gifUrl": ""
             };
             this.expectedResponse = {
-                "id": "3",
+                "id": "6",
                 "title": `${this.testPost.title}`,
                 "text": `${this.testPost.text}`,
                 "gifUrl": `${this.testPost.gifUrl}`,
@@ -136,9 +144,21 @@ describe('api routes', () => {
                 throw new Error(`Put request failed. Status code ${putRequest.status}: ${putRequest.text}`);
             };
         });
+        test("PUT /gitpush/laughing with wrong adjust value returns error", async () => {
+            const requestBody = {
+                "id": "2",
+                "adjust": "addition"
+            }
+            // Make PUT request
+            const putRequest = await request(api)
+                .put('/gitpush/laughing')
+                .send(requestBody);
+            expect(putRequest.status).toBe(400);
+            expect(JSON.parse(putRequest.text).message).toContain('adjust must be "add" or "remove"');
+        })
     });
 
-    describe("DELETE reqeusts successfully remove specified post/comment", () => {
+    describe("DELETE requests successfully remove specified post/comment", () => {
         test("DELETE /gitrm/2 successfully removes post #2 and updates ids", async () => {
             // Note that the post added in the earlier test still exists in this test as post 3
             const deletePost = await request(api).delete('/gitrm/2');
@@ -147,7 +167,7 @@ describe('api routes', () => {
                 const checkPosts = await request(api).get('/pushes');
                 const posts = checkPosts.body;
                 // Check the post was deleted
-                expect(posts.length).toBe(2);
+                expect(posts.length).toBe(5);
                 // Check that post ids were updated
                 expect(posts[posts.length-1].id).toBe(posts.length);
             } else {
@@ -183,5 +203,13 @@ describe('api routes', () => {
             };
         });
     });
+
+    describe("Errors in routes are handled", () => {
+        test("PUT /gitpush/:emoji with an invalid emoji returns a response asking for the correct path", async () => {
+            const response = await request(api).put('/gitpush/thumbs');
+            expect(response.status).toBe(404);
+            expect(response.text).toBe("Please specify /thumbsUp, /thumbsDown, or /laughing in fetch url");
+        })
+    })
 });
 
