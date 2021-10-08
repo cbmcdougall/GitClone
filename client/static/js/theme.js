637b01b1165a5ebae7ec8609d1d50f2150cf6f61
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-10-08 11:12:53 +0100

    fix accidental overwrite of code

--- client/static/js/theme.js
+++ client/static/js/theme.js
@@ -3,20 +3,27 @@ const moonIcon = document.querySelector('.sun-moon-theme');
 const moon = document.getElementById('moon');
 const sun = document.getElementById('sun');
 
-// moonIcon.addEventListener('click', () => {
-//     document.body.classList.toggle('dark-theme');
-//     moon.classList.toggle('hide-icon');
-//     sun.classList.toggle('hide-icon');
+// Load dark theme if it was toggled on
+const darkThemeOn = sessionStorage.getItem("darktheme")
+if (darkThemeOn==="true"){
+    document.body.className = "dark-theme";
+    moon.classList.add('hide-icon');
+    sun.classList.remove('hide-icon');
+}
 
-// })
-
-moonIcon.addEventListener('click', toggleThemes);
-
-function toggleThemes() {
+moonIcon.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
     moon.classList.toggle('hide-icon');
     sun.classList.toggle('hide-icon');
-}
+    // Store toggle info between pages
+    if (document.body.className.includes("dark-theme")){
+        // Currently in dark mode
+        sessionStorage.setItem("darktheme", "true")
+    } else {
+        // Currently in light mode
+        sessionStorage.setItem("darktheme", "false")
+    }
+})
 ////////////////////////////////////////////////
 ///Search bar
 
