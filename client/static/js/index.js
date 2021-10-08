e6d4e381434a7627337483f597a98d0283c88987
Saja A <saja.aabith@gmail.comxitcdgit config --global user.name saja-aabith>
2021-10-08 08:35:45 +0100

    minor

--- client/static/js/index.js
+++ client/static/js/index.js
@@ -61,7 +61,6 @@ function delay(callback, ms) {
     };
   }
   
-
 const gifSearch = document.getElementById('gifQuery')
 const gifLink = document.getElementById('gifLink')
 const gifImg = document.getElementById('gif')
@@ -76,7 +75,6 @@ gifSearch.addEventListener('keyup', delay(function (e) {
         gifImg.src=link;
         gifLink.value=link;
     })
-
 }, 1000))
 
 
